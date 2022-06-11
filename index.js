917774a2edbe15c7989389c1fe23bdba02e2daae
Simeon <olabs06@yahoo.com>
2022-06-11 16:13:04 +0100

    modifying index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(myActivity) {
+    if (myActivity === undefined) {
+        return ("This Saturday, I want to roller-skate!");
+    } else {
+        return (`This Saturday, I want to ${myActivity}!`);
+    }
+               
+}
+
+
+function mondayWork(myActivity) {
+    if (myActivity === undefined){
+    return "This Monday, I will go to the office.";
+  } else {
+    return (`This Monday, I will ${myActivity}.`);
+  }
+}
+
+
+function wrapAdjective(visualFlair = "*") {
+
+  return function (params = "special") {
+    return `You are ${visualFlair}${params}${visualFlair}!`
+  } 
+   
+}
